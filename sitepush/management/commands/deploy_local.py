7c696a18b74e37e06e1f9b751418225e5e52081f
Yuri Prezument <y.prezument@gmail.com>
2012-01-15 21:15:34 +0200

    added management commands (imported from BG)

---
+++ sitepush/management/commands/deploy_local.py
@@ -0,0 +1,50 @@
+from django.conf import settings
+from django.core import management
+from django.core.management.base import BaseCommand
+
+from fabric.api import local
+from optparse import make_option
+
+
+class Command(BaseCommand):
+    help = "Redeploy the current instance"
+
+    option_list = BaseCommand.option_list + (
+        make_option('--servername', action='store', type='string',
+                    dest='servername', default='staging',
+                    help='Server name - to pick which configuration to use'),
+        make_option('--noreqs', action='store_true', dest='noreqs',
+                    default=False, help='Don\'t update requirements'),
+    )
+
+    def handle(self, *args, **options):
+        server_name = options.get('servername')
+
+        noreqs = options.get('noreqs')
+
+        if not server_name in settings.DEPLOYS:
+            self.stderr.write('Non existant server config')
+            return
+
+        server = settings.DEPLOYS[server_name]
+
+        local('git pull origin {0}'.format(server['BRANCH']))
+
+        if not noreqs:
+            local('pip install -r requirements.txt')
+
+        management.call_command('syncdb',
+                interactive=False, stdout=self.stdout)
+        management.call_command('migrate',
+                interactive=False, stdout=self.stdout)
+        management.call_command('collectstatic',
+                interactive=False, stdout=self.stdout)
+
+        # Reload the app in the webserver
+        ws = server['WEBSERVER']
+        if ws == 'apache':
+            local('touch deploy/wsgi.py')  # reload wsgi
+        elif ws == 'gunicorn':
+            local('sudo kill -HUP `cat {0}`'.format(server['PIDFILE']))
+        else:
+            self.stderr.write('Unknown webserver type!')
---
+++ sitepush/management/commands/deploy_remote.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+
+from fabric.api import run, cd, env, prefix
+from optparse import make_option
+from django.conf import settings
+import time
+
+
+class Command(BaseCommand):
+    help = """Redeploy the application on a remote server"""
+    args = "<server_name server_name ...>"
+
+    option_list = BaseCommand.option_list + (
+        make_option('--noreqs', action='store_true', dest='reqs',
+                    default=False, help='Update requirements'),
+    )
+
+    def handle(self, *args, **options):
+        noreqs = options.get('reqs')
+        if not args:
+            self.stderr.write('No servers selected\n')
+            return
+
+        for n in args:
+            if n in settings.DEPLOYS:
+                self.deploy(settings.DEPLOYS[n], n, noreqs)
+            else:
+                self.stderr.write('Error - server {0} not found\n'.format(n))
+
+    def deploy(self, server, server_name, noreqs=False):
+        starttime = time.time()
+        env.host_string = '{0}@{1}'.format(server['USER'], server['HOST'])
+        self.stdout.write('Depoloying to "{0}"...\n'.format(server_name))
+
+        with cd(server['DIR']), prefix('workon {0}'.format(server['ENV'])):
+            cmd = ('python manage.py deploy_local --servername={servername} '
+                   '{noreqs} --settings={settings}'). \
+                   format(noreqs='--noreqs' if noreqs else '',
+                          settings=server['SETTINGS'] if 'SETTINGS' in server
+                                                      else 'settings',
+                          servername=server_name)
+            run(cmd)
+
+        self.stdout.write('Deployed successfully\n')
+        self.stdout.write('Time elapsed: {0} seconds\n\n'.\
+                format(time.time() - starttime))
